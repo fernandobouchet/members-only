108d6a81e7eb93b6d6a17fbb202beb83e4a283c4
fernandobouchet <fernandobouchet@gmail.com>
2022-10-13 21:40:49 -0300

    'user' model, add missing field

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = mongoose.Schema(
     username: { type: String, required: [true, 'Please add a username'] },
     lastname: { type: String, required: [true, 'Please add a lastname'] },
     email: { type: String, required: [true, 'Please add an email'] },
+    member_status: { type: String, default: 'normal' },
     password: { type: String, required: [true, 'Please add a password'] },
   },
   {
